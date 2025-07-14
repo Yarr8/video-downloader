7b1a49dccdd169a31f3789a15fb196ad78057bc7
Arsenii <ars3577@gmail.com>
2025-07-14 12:53:04 +0300

    feat(api-gateway): add base api-gateway service (#4)

---
+++ api-gateway/src/test/java/com/video_downloader/api_gateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.video_downloader.api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
